a4a4e3d1c8ec02dff28b52c15cd9b32ca0fc2ab5
Lee Trani <LMT337@gmail.com>
2017-09-05 14:21:46 -0500

    created rcmnd function

--- qcfailrec.tm.py
+++ qcfailrec.tm.py
@@ -8,10 +8,13 @@ parser.add_argument("file", type=str)
 args = parser.parse_args()
 outfile = args.file + '.reccomendation.tsv'
 
+def topmed_metrics_fail(line):
+
+
 with open(args.file) as infile, open( outfile, 'w') as outfilecsv:
     print(infile)
     reader = csv.DictReader(infile, delimiter="\t")
     print(reader)
     for line in reader:
-        print(line)
+        topmed_metrics_fail(line)
 exit()
