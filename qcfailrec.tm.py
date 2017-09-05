cb8c58171d0f0b6401397d2a0ae4891fb85a3c8f
Lee Trani <LMT337@gmail.com>
2017-09-05 13:39:59 -0500

    input file

--- qcfailrec.tm.py
+++ qcfailrec.tm.py
@@ -1,1 +1,17 @@
 import csv
+import argparse
+parser = argparse.ArgumentParser()
+
+
+parser.add_argument("file", type=str)
+
+args = parser.parse_args()
+outfile = args.file + '.reccomendation.tsv'
+
+with open(args.file) as infile, open( outfile, 'w') as outfilecsv:
+    print(infile)
+    reader = csv.DictReader(infile, delimiter="\t")
+    print(reader)
+    for line in reader:
+        print(line)
+exit()
