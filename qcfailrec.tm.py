1e60ddffdc72597e7ef4e238f576ba6993cd1e3d
Lee Trani <LMT337@gmail.com>
2017-09-06 12:38:35 -0500

    added print function

--- qcfailrec.tm.py
+++ qcfailrec.tm.py
@@ -17,6 +17,8 @@ header = ['WorkOrder','date_QC','DNA','instrument_data_count','instrument_data_i
           'PF_ALIGNED_BASES','TOTAL_PERCENT_DUPLICATION','TOTAL_READS','reads_mapped_as_singleton_percentage',
           'reads_mapped_in_proper_pairs_percentage','PF_HQ_ALIGNED_Q20_BASES','STATUS','Flagstats','QC Failed Metrics']
 
+qc_header = ['reason', 'qc_comment', 'qc_reccomendation']
+
 qc = {}
 
 def topmed_metrics_fail(line):
@@ -37,17 +39,27 @@ def topmed_metrics_fail(line):
         qc['qc_reccomendation'] = 'Possible library/DNA swap, needs further investigation starting with QC'
         print('GTother-True')
 
-        elif (float(line['GENOTYPING_CHIPMIX']) >= 0.9):
+    elif (float(line['GENOTYPING_CHIPMIX']) >= 0.9):
         qc['reason'] = 'genotyping_chipmix greater than 0.9'
         qc['qc_comment'] = 'Coverage fail'
         qc['qc_reccomendation'] = 'Possible library/DNA swap, needs further investigation starting with QC'
         print('GTother-True')
 
+    elif (float(line['HAPLOID_COVERAGE']) < 30 or float(line['TOTAL_BASES_Q20_OR_MORE']) < 86
+          or float(line['PCT_10X']) < 0.95 or float(line['PCT_20X']) < 0.90):
+        print('hello')
 
 with open(args.file) as infile, open( outfile, 'w') as outfilecsv:
     reader = csv.DictReader(infile, delimiter="\t")
+
+    header_fields = header + qc_header
+    w = csv.DictWriter(outfilecsv, header_fields, delimiter="\t")
+    w.writeheader()
+
     for line in reader:
         topmed_metrics_fail(line)
-        print(qc)
+        results = dict(list(line.items()) + list(qc.items()))
+        w.writerow(results)
+
 
 exit()
